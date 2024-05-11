1b214ba357e9b8384d85904c40e002c74fa320ab
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-05-11 22:52:18 +0300

    public

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,8 +69,10 @@ return [
     |
     */
 
+
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        base_path('public_html') => base_path('public'),
     ],
 
 ];
