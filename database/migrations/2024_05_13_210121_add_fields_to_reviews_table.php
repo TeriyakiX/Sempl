0841b61e74b29e5acc0c249bb0b636dc0de8556c
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-07-12 08:22:30 +0300

    migrations

---
+++ database/migrations/2024_05_13_210121_add_fields_to_reviews_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->text('pros')->nullable();
+            $table->text('cons')->nullable();
+            $table->json('media')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropColumn('pros');
+            $table->dropColumn('cons');
+            $table->dropColumn('media');
+        });
+    }
+};
