945c5946cf7b6bcd73f9ac5d53d115d455c1a58c
Данил Денисович Демидов <dddemidov@lab-207-11.tomtit-tomsk.ru>
2024-10-10 14:58:55 +0700

    h1

--- app/Http/Controllers/AddressSuggestionController.php
+++ app/Http/Controllers/AddressSuggestionController.php
@@ -12,24 +12,15 @@ class AddressSuggestionController extends Controller
     public function suggest(Request $request)
     {
         $query = $request->input('query');
-        $count = $request->input('count', 5);
+        $count = $request->input('count', 5); // Количество подсказок, по умолчанию 5
 
         if (empty($query)) {
             return response()->json(['error' => 'Запрос не может быть пустым'], 400);
         }
+
         // Получаем подсказки из DaData
         $suggestions = DaDataAddress::prompt($query, $count, Language::RU);
 
-        // Фильтруем поля
-        $filteredSuggestions = array_map(function ($suggestion) {
-            return [
-                'city' => $suggestion['data']['city'],
-                'street' => $suggestion['data']['street'],
-                'house' => $suggestion['data']['house'],
-            ];
-        }, $suggestions);
-
-        return response()->json(['suggestions' => $filteredSuggestions]);
-
+        return response()->json($suggestions);
     }
 }
