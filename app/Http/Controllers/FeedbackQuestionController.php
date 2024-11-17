5501bea532cd9d8d08b0db4b2603ad10d47b5344
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-11-17 22:53:27 +0300

    update question

--- app/Http/Controllers/FeedbackQuestionController.php
+++ app/Http/Controllers/FeedbackQuestionController.php
@@ -16,10 +16,17 @@ class FeedbackQuestionController extends Controller
 
     public function createAnswer(Request $request, $questionId)
     {
+        $validated = $request->validate([
+            'answer' => 'required|string',
+            'feedback_id' => 'required|integer',
+        ]);
+
         $answer = FeedbackAnswer::create([
             'question_id' => $questionId,
-            'answer' => $request->answer,
+            'answer' => $validated['answer'],
+            'feedback_id' => $validated['feedback_id'],
         ]);
+
         return response()->json($answer, 201);
     }
 
