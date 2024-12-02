bd5c6ef7f89a0b2a3f38bf9e59e4a710bb8c0d29
DaNyQ1Q <demidov.danil.fuul@gmail.com>
2024-12-02 09:13:15 +0300

    update game

--- app/Http/Resources/ProductFeedbackAnswerResource.php
+++ app/Http/Resources/ProductFeedbackAnswerResource.php
@@ -10,8 +10,6 @@ class ProductFeedbackAnswerResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'question_id' => $this->question_id,
-            'answer_id' => $this->answer_id,
             'answer' => $this->answer,
         ];
     }
